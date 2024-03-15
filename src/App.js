9b456b1d687da6010974da0d84a15a71a2d2cac8
pigm6080 <pigm6080@gmail.com>
2024-03-15 12:10:43 +0900

    34p 모듈화 시작

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
 
 function App() {
+
+
+
+
+  //랜더링 하는 부분
   return (
     <div className="App">
-      
-      Hello React !
+      <header>
+        <h1><a href = "/"> WEB</a></h1>
+      </header>
+      <nav>
+        <ol>
+          <li><a href='/read/1'>html</a></li>
+          <li><a href='/read/2'>css</a></li>
+          <li><a href='/read/3'>js</a></li>
+        </ol>
+
+      </nav>
+
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB
+      </article>
     
     </div>
   );
 }
 
-export default App;
+export default App; //내보낼때는 이걸로. (수출)
